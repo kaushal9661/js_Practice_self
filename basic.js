96e476b1f54a4bdf8a21ad36f2f56041117d35e5
kaushal9661 <kaushalkr0004@gmail.com>
2025-06-28 11:18:30 +0000

    Container Confige done

---
+++ basic.js
@@ -0,0 +1,10 @@
+console.log("Ram");
+function getName(){
+    console.log("Raja Kumar")
+    console.log("Mohan Kumar");
+    console.log("Ritesh Kumar");
+    console.log("Amit Mani Kumar");
+}
+getName();
+console.log(" ")
+getName();
